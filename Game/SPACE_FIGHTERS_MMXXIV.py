3a8949717cdc9470b89addf2a6e5dd859847494d
faisalmujawar148 <98691778+faisalmujawar148@users.noreply.github.com>
2024-03-28 00:16:11 +0000

    Update SPACE_FIGHTERS_MMXXIV.py

--- Game/SPACE_FIGHTERS_MMXXIV.py
+++ Game/SPACE_FIGHTERS_MMXXIV.py
@@ -1,13 +1,14 @@
 try:
     import simplegui
+   
 except ImportError:
+   
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 import random
 import math
-
 #Img constants
 CANVAS_DIMS = (1024, 768)
-bullet_radius = 5
+bullet_radius = 8
 ship_scale = (70, 70)
 asteroid_scale = (100, 100)
 IMG1 = simplegui.load_image('https://i.ibb.co/yXPtX31/spaceship1.png')
@@ -20,6 +21,7 @@ IMG2_CENTRE = (IMG2_DIMS[0]/2,IMG2_DIMS[1]/2)
 #Credit SlashDashGames Studio for the Background
 BACKGROUND = simplegui.load_image('https://i.postimg.cc/9WRzjmwB/bggif.png')
 BACKGROUND_DIMS = (1024, 768)
+#Asteroid by Kim Lathrop
 #MIDGROUND = simplegui.load_image('http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/debris2_blue.png')
 MIDGROUND = simplegui.load_image('https://i.ibb.co/SnNcDCt/midground-2x-RIFE-RIFE4-0-41-332fps-ezgif-com-gif-to-sprite-converter.png')
 MIDGROUND_DIMS = (640, 480)
@@ -53,7 +55,7 @@ def hit_timer(canvas, duration):
         #simplegui.Sound("https://file.io/QRe9MaY47j4h").rewind()
         #simplegui.Sound("https://file.io/QRe9MaY47j4h").play()
         ship.isHit = False
-        
+       
 def randomVel(pos):
     if pos[0] >= CANVAS_DIMS[0]/2:
         x = random.randint(-3,-1)
@@ -88,16 +90,16 @@ class Border:
     def __init__(self,img,dims):
         self.img = img
         self.dims = dims
-        
+       
     def draw(self,canvas):
         val = (CANVAS_DIMS[0]/2,CANVAS_DIMS[1]/2)
         canvas.draw_image(border.img, (self.dims[0]/2, self.dims[1]/2), self.dims, val, (1029, 768))
-        
+       
 class Midground:
     def __init__(self,img,dims):
         self.img=img
         self.dims=dims
-        
+       
     def draw(self, canvas):
         global bg_counter
         global move_x
@@ -111,7 +113,7 @@ class Midground:
             move_x-=0.2
         if move_x<=0:
             move_x+=1028
-            
+           
 class Background:
     def __init__(self,img,dims,columns,rows):
         self.img=img
@@ -124,18 +126,19 @@ class Background:
         self.frame_height = self.height/self.rows
         self.frame_centre = (self.frame_width/2, self.frame_height/2)
         self.frame_index = [0,0]
-        
+       
     def draw(self, canvas):
         global bg_counter
         global offset
         width_height_source = (1024,758)
+        center_source = (1024,758)
         center_source = (self.frame_width*self.frame_index[0]+self.frame_centre[0], self.frame_height*self.frame_index[1]+self.frame_centre[1])
         width_height_source = (self.frame_width, self.frame_height)
-        
+       
         canvas.draw_image(background.img, center_source, width_height_source, (CANVAS_DIMS[0]/2,CANVAS_DIMS[1]/2), (1024, 758))
         if bg_counter % 10 == 0:
             self.next_frame()
-        
+       
     def next_frame(self):
         self.frame_index[0] += 1
         if self.frame_index[0] == self.columns:
@@ -189,7 +192,7 @@ class Spaceship:
         self.position = position
         self.hp = hp
         self.isHit = False
-        
+       
     def move(self, canvas):
         #thrusters animation and optionally particle effects for that
         global ship_angle
@@ -197,7 +200,7 @@ class Spaceship:
     def draw(self, canvas):
         global ship_angle
         canvas.draw_image(self.img, self.img_centre, self.img_dims, camera(self.position), ship_scale, ship_angle-55)
-        
+       
     def update(self, canvas):
         global offset
         global score
@@ -213,7 +216,7 @@ class Spaceship:
             frame3.set_mouseclick_handler(Spaceship.mouse_handler)
             frame3.set_draw_handler(self.game_over)
             frame3.start()
-    
+   
     def mouse_handler(pos):
         global bullets
         global asteroids
@@ -240,7 +243,7 @@ class Spaceship:
         canvas.draw_polygon((camera((self.position[0]+30,self.position[1]+20)),camera((self.position[0]+70,self.position[1]+50))),1,'white','white')
         canvas.draw_text(text, camera((self.position[0]+70,self.position[1]+60)), 28, 'white')
         hit_timer(canvas, 180)
-        
+       
 class Asteroid:
     def __init__(self, img, img_centre, img_dims, position, vel, rotation):
         self.img = img
@@ -251,7 +254,7 @@ class Asteroid:
         self.rotation = rotation
         self.isHit = False
         self.lifespan = 0
-        
+       
     def draw(self, canvas):
         canvas.draw_image(self.img, self.img_centre, self.img_dims, camera(self.position), asteroid_scale, self.rotation)
 
@@ -259,13 +262,13 @@ class Asteroid:
         self.position = (self.position[0] + self.vel[0], self.position[1] + self.vel[1])
         self.lifespan += 1
         self.draw(canvas)
-        
+       
 class Bullet:
     def __init__(self, position, vel, rotation):
         self.position = position
         self.vel = vel
         self.rotation = rotation
-        
+       
     def draw(self, canvas):
         canvas.draw_circle(camera(self.position), bullet_radius, 1, "yellow", "red")
 
@@ -277,11 +280,11 @@ class Interaction:
     def __init__(self, keyboard, spaceship):
         self.keyboard = keyboard
         self.booster_velocity = -8  # Initial jump velocity
-        self.accel = 0.5 
+        self.accel = 0.5
         self.max_vel = 60  # Maximum jump height
         self.friction = 0.9
         self.spaceship = spaceship
-                          
+                         
     def button_clicked():
         #Create a frame and assign callbacks to event handlers
         global score
@@ -293,7 +296,7 @@ class Interaction:
         frame2.set_keydown_handler(keyboard.keydown_handler)
         frame2.set_keyup_handler(keyboard.keyup_handler)
         frame2.start()
-        
+       
     def draw(canvas):
         canvas.draw_polygon([(button_pos[0] - button_width/2, button_pos[1] - button_height/2),
                              (button_pos[0] - button_width/2, button_pos[1] + button_height/2),
@@ -301,7 +304,7 @@ class Interaction:
                              (button_pos[0] + button_width/2, button_pos[1] - button_height/2)],
                             1, "White", "Blue")
         canvas.draw_text("START", (button_pos[0] - button_width/4, button_pos[1] + button_height/4), 24, "White")
-        
+       
     def welcome(canvas):
         canvas.draw_text("Welcome to Space Fighters!", [65, 90], 45, "Yellow")
         canvas.draw_text("CAN YOU DEFEAT THE ASTEROIDS?", [70, 150], 20, "Blue")
@@ -310,7 +313,7 @@ class Interaction:
         canvas.draw_text("Are you up for the challenge?", [70, 270], 20, "Blue")
         canvas.draw_text("Click the button to start", [70, 320], 20, "Purple")
         Interaction.draw(canvas)
-    
+   
     def mouse_handler(pos):
         global bullets
         global asteroids
@@ -321,7 +324,7 @@ class Interaction:
             bullet_cd.start()
             timer.start()
             Interaction.button_clicked()
-    
+   
     def update(self, canvas):
         global ship_angle
         global speed_x
@@ -355,7 +358,7 @@ class Interaction:
             speed_x *= self.friction
             speed_y *= self.friction
             shooting = False
-        
+       
         self.spaceship.position = (self.spaceship.position[0] + speed_x * 0.03, self.spaceship.position[1] + speed_y * 0.03)
         ship_angle %= 2 * math.pi
         if self.spaceship.position[0] < -1000:
@@ -372,7 +375,6 @@ class Interaction:
 class Collisions1:
     def __init__(self):
         pass
-
     def update(self, canvas):
         global text
         global ship
@@ -387,14 +389,15 @@ class Collisions1:
                     asteroids[i].isHit = True
                     ship.isHit = True
                     text=random.choice(["L bozo", "-1", "Checkmate in 1", "!! Brilliant move","XD"])
-                    print('hit')             
+                    print('hit')            
                     ship.hp -=1
                     print(ship.hp)
                     #explode(canvas, asteroids[i].position)
                     ship.hit(canvas, text)
+                 
         for i in range(len(debris)):
-            asteroids.pop(debris[i])         
-            
+            asteroids.pop(debris[i])        
+           
 class Collisions2:
     def __init__(self):
         pass
@@ -417,7 +420,7 @@ class Collisions2:
         for i in range(len(debris[0])):
             bullets.pop(debris[0][i])
             asteroids.pop(debris[1][i])
-            
+           
 class Scoreboard:
     def __init__(self):
         pass
@@ -453,12 +456,15 @@ def bullet_cd_handler():
 
 def timer_handler():
     global asteroids
-    if len(asteroids) < 50:
+    if len(asteroids) < 200:
         pos = randomPos()
         vel = randomVel(pos)
         asteroids.append(Asteroid(IMG2, IMG2_CENTRE, IMG2_DIMS, pos, vel, randomRot()))
 bullet_cd = simplegui.create_timer(100, bullet_cd_handler)
-timer = simplegui.create_timer(2000, timer_handler)
+asteroid_timer = 2000 - score
+if (asteroid_timer) > 500:
+    asteroid_timer = 500
+timer = simplegui.create_timer(asteroid_timer, timer_handler)
 
 # Drawing handler
 def draw(canvas):
@@ -466,46 +472,32 @@ def draw(canvas):
     global score
     bg_counter += 1
     out_range = [[], []]
-    
+   
     background.draw(canvas)
     midground.draw(canvas)
-    
+   
     for i in range(len(bullets)):
         bullets[i].update(canvas)
         if (bullets[i].position[0]-offset[0] < 0)|(bullets[i].position[0]-offset[0] > CANVAS_DIMS[0])|(bullets[i].position[1]-offset[1] < 0)|(bullets[i].position[1]-offset[1] > CANVAS_DIMS[1]):
             out_range[0].append(bullets[i])
     for i in range(len(out_range[0])):
         bullets.remove(out_range[0][i])
-        
+       
     for i in range(len(asteroids)):
         asteroids[i].update(canvas)
         if asteroids[i].lifespan > 1800:
             out_range[1].append(i)
     for i in range(len(out_range[1])):
         asteroids.pop(out_range[1][i])
-    
-    
+   
+   
     score += 1/30
     collisions1.update(canvas)
+
     collisions2.update()
     interaction.update(canvas)
-    
-    
-    
+   
+   
+   
     ship.update(canvas)
-    edge.draw(canvas)
-    border.draw(canvas)
-    scoreboard.update(canvas)
-    if ship.isHit:
-        ship.hit(canvas, text)
-
-    if ship.hp < 1:
-        ship.hp = 10
-        frame2.stop()
-
-# Create a frame and assign callbacks to event handlers
-frame1 = simplegui.create_frame("Main Menu", CANVAS_DIMS[0], CANVAS_DIMS[1])
-frame1.set_canvas_background('Black')
-frame1.set_draw_handler(Interaction.welcome)
-frame1.set_mouseclick_handler(Interaction.mouse_handler)
-frame1.start()
+    edge.draw(canv
